7684202141b7d43111af554ecdf063017714e0d5
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-03-30 23:58:43 +0000

    Start of image-slider exercise

---
+++ image-slider/index.js
@@ -0,0 +1,12 @@
+let nextBtn = document.getElementById('next-btn');
+let prevBtn = document.getElementById('previous-btn');
+
+let sliderImages = document.getElementsByClassName('slider-image');
+let currentSliderImage = 1;
+
+nextBtn.addEventListener('click', () => {
+    currentSliderImage++;
+    for (let i = 0; i < sliderImages.length; i++) {
+
+    }
+});
