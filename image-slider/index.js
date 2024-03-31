1e4710d3a0239ee5fdc2c0e952379c5f315b21ba
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-03-31 00:15:49 +0000

    Finished image-slider exercise

--- image-slider/index.js
+++ image-slider/index.js
@@ -2,11 +2,31 @@ let nextBtn = document.getElementById('next-btn');
 let prevBtn = document.getElementById('previous-btn');
 
 let sliderImages = document.getElementsByClassName('slider-image');
-let currentSliderImage = 1;
+let currentSliderImage = 0;
+
+function updateShownImage(shownImageIndex) {
+    for (let i = 0; i < sliderImages.length; i++) {
+        let image = sliderImages[i];
+        if (i === shownImageIndex) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    }
+}
 
 nextBtn.addEventListener('click', () => {
     currentSliderImage++;
-    for (let i = 0; i < sliderImages.length; i++) {
-
+    if (currentSliderImage === sliderImages.length) {
+        currentSliderImage = 0;
     }
+    updateShownImage(currentSliderImage);
+});
+
+prevBtn.addEventListener('click', () => {
+    currentSliderImage--;
+    if (currentSliderImage < 0) {
+        currentSliderImage = sliderImages.length - 1;
+    }
+    updateShownImage(currentSliderImage);
 });
